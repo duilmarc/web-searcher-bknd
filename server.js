de1556b46b7ee0a6edf49d5925e919eb3a15f1c5
duilmarc <franco.chp@gmail.com>
2020-11-12 06:53:44 -0500

    deploy:heroku:config

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const server = express();
+const cors = require("cors");
+server.use(express.json());
+server.use(cors());
+
+app.use(express.static(__dirname + '/dist/bweb-searcher-bk'));
+server.get("/search/:word", async (req,res) => {
+    try{
+        const { word } = req.params;
+        var request = require('request');
+        request('https://us-central1-web-searcher-293217.cloudfunctions.net/query?word='+word+'&limit=-1', function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+            var info = JSON.parse(body);
+            return res.send({ error : false , data : info});
+            }
+    })
+    }
+    catch (err) {
+        next(err);
+      }
+    
+});
+
+server.listen( process.env.PORT || 8000, () => {
+   
+});
