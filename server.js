7502c9749a8ae7713a7fcbacd87567cf6545dc9d
duilmarc <franco.chp@gmail.com>
2020-11-12 11:30:28 -0500

    deploy

--- server.js
+++ server.js
@@ -6,19 +6,16 @@ server.use(cors());
 
 server.use(express.static(__dirname + '/dist/bweb-searcher-bk'));
 server.get("/search/:word", async (req,res) => {
-    try{
         const { word } = req.params;
         var request = require('request');
-        request('https://us-central1-web-searcher-293217.cloudfunctions.net/query?word='+word+'&limit=-1', function (error, response, body) {
+        await request('https://us-central1-web-searcher-293217.cloudfunctions.net/query?word='+word+'&limit=-1', function (error, response, body) {
             if (!error && response.statusCode == 200) {
             var info = JSON.parse(body);
             return res.send({ error : false , data : info});
             }
     })
-    }
-    catch (err) {
-        next(err);
-      }
+    
+
     
 });
 
