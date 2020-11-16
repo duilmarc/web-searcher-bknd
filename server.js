db08504e067615940d93de30509f393208f01485
Franco Duilmarc Chavez Ponce <franco.chp@gmail.com>
2020-11-16 12:07:18 -0500

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ server.use(cors());
 
 server.get("/search/:word", async (req,res) => {
         const { word } = req.params;
-        var request = require('request');
+        var request = require("request");
         await request('https://us-central1-web-searcher-293217.cloudfunctions.net/query?word='+word+'&limit=-1', function (error, response, body) {
             if (!error && response.statusCode == 200) {
             var info = JSON.parse(body);
